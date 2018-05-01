74f2fb47a0d693b3015ef417a15ba3f249786a85
Paulina Salem <paulinasalem@yahoo.com>
2018-05-01 00:15:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 def prime?(num)
-  if num<=1
-    false
+    if num<=1
+      false
+    end
+      n=2
+  while n<num
+    if num%n==0
+      false
+    end
+    n+=1
   end
-    n=2
-while n<num
-  if num%n==0
-    false
-  end
-  n+=1
-end
 end
